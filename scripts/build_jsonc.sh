265feaa015bf5c70c9533afe58e1ff4eed330101
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2019-04-19 12:20:14 +0200

    [+] my and jsonc submodules, .gitmodules | [~] build_jsonc.sh
    build_jsonc.sh :
    - Remove cloning operations

--- scripts/build_jsonc.sh
+++ scripts/build_jsonc.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "lib/jsonc" ]]; then
-    cd lib
-    wget https://github.com/json-c/json-c/archive/json-c-0.13.1-20180305.zip
-    unzip *.zip
-    rm *.zip
-    mv json-c-* jsonc
-    cd jsonc
+if [[ ! -d "lib/json-c" ]]; then
+    cd lib/jsonc
     sh autogen.sh
     ./configure
     cmake .
